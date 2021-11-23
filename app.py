bdb6658070ce699d248b54e37bdb1488341c8994
Paulappz <pauloluyege@gmail.com>
2021-11-23 01:31:32 +0100

    updates

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask('hello')
 @app.route('/')
 def hello():
-  return "Hello World!\n"
+  return "Hello Paul!\n"
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
